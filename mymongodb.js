c2854603643bf2601db8c635f83d6db2c1943909
Pete Wood <pete@petewood.net>
2015-08-31 15:29:40 +0100

    change to zz

--- mymongodb.js
+++ mymongodb.js
@@ -5,7 +5,7 @@ var MongoClient = require('mongodb').MongoClient;
 var user1 = {
     name: 'modulus admin',
     age: 42,
-    roles: ['adminpeteyy', 'moderator', 'user']
+    roles: ['adminpetezz', 'moderator', 'user']
 };
 var user2 = {
     name: 'modulus user',
