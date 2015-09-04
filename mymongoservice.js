52b2b4163a1067200a41173ac7101beeb80020dd
Pete Wood <pete@petewood.net>
2015-09-04 17:40:39 +0100

    Working JSON service response - however, it repeats the output

--- mymongoservice.js
+++ mymongoservice.js
@@ -1,6 +1,6 @@
-// Retrieve
-var MongoClient = require('mongodb').MongoClient;
 
+// Retrieve the data back from MongoDB
+var MongoClient = require('mongodb').MongoClient;
 // Connect to the db
 MongoClient.connect("mongodb://localhost:27017/hellpete", function(err, db) {
     if (!err) {
@@ -32,4 +32,19 @@ MongoClient.connect("mongodb://localhost:27017/hellpete", function(err, db) {
 }
      
 });
+
+//Create a service to serve up the data
+var http = require('http');
+
+// Configure our HTTP server to respond with Hello World to all requests.
+var server = http.createServer(function (request, response) {
+  response.writeHead(200, {"Content-Type": "text/plain"});
+  response.end("Hello World\n");
+});
+
+// Listen on port 8000, IP defaults to 127.0.0.1
+server.listen(8000);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:8000/");
 
